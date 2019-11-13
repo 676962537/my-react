a17cb02aac9213da098647b3c75e76b8b6e86525
mahenan <676962537@qq.com>
2019-11-13 11:25:28 +0800

    change

--- .idea/fileTemplates/JSX File.jsx
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-import { render,findDOMNode } from 'react-dom';
-class Com extends Component {
-  render() {
-    return (<div></div>)
-  }
-}
-export default Com
